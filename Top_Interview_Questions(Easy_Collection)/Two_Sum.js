dd22e977afcda7faa106d311612a5d17751533d1
Morol <morolswediu@gmail.com>
2021-10-19 09:53:42 +0600

    Two Sum

--- Top_Interview_Questions(Easy_Collection)/Two_Sum.js
+++ Top_Interview_Questions(Easy_Collection)/Two_Sum.js
@@ -1,3 +1,5 @@
+// Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546
+
 const twoSum = function(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     const pos = nums.findIndex((num, idx) => num === target - nums[i] && idx !== i);
