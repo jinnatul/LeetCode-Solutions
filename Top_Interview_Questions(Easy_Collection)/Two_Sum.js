ef98514ed41f34d50c15b72b767d72a7ba1b3b5f
Morol <morolswediu@gmail.com>
2021-10-19 09:52:20 +0600

    Two Sum

---
+++ Top_Interview_Questions(Easy_Collection)/Two_Sum.js
@@ -0,0 +1,12 @@
+const twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const pos = nums.findIndex((num, idx) => num === target - nums[i] && idx !== i);
+    if (pos !== -1) {
+      return [i, pos];
+    }
+  }
+};
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
